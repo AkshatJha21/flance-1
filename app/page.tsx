45d772f5ecf64261674944d936ed6ff3ae854a93
AkshatJha21 <akshatj2134@gmail.com>
2024-09-28 22:07:18 +0530

    lp 28/09

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,8 @@
 export default function Home() {
   return (
     <div className="bg-[#131313] text-[#EDE6D6] h-screen flex items-center justify-center">
-      Sarvatva Web
+      Tad Ekam
+      Inspired by the Rig Veda
     </div>
   );
 }
