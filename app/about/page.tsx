92c085274324d864f1294a284a842c53146f1bd0
AkshatJha21 <akshatj2134@gmail.com>
2024-10-14 08:10:30 +0530

    14/10

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const About = () => {
   return (
     <div className="bg-[#131313] text-[#EDE6D6] h-screen flex items-center justify-center">
-      The Craft of Wholeness
+      Craft of Wholeness
     </div>
   )
 }
